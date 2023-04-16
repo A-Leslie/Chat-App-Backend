266f56e440ceb4315ad49411de80f1a9cb28d289
_l.esli.e_ <anneuhiriwe@gmail.com>
2023-04-16 13:56:23 +0200

    end

---
+++ connect.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+var mongoURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PW}@cluster0.ggnf1l5.mongodb.net/mernchat-app-user?retryWrites=true&w=majority`
+
+const connectToMongo =  () => {
+     mongoose.connect(mongoURI)
+     .then( ()=>
+        console.log("Connected successfully")
+    )
+}
+
+
+module.exports = connectToMongo;
